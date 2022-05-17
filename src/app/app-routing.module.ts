054182191f71f05a660e1861a7afb0fb351e3d8c
Ramalakshmanan.S <srlaxman1497@gmail.com>
2022-05-17 21:53:18 +0530

    angular capstone 1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "login/:role", component: LoginComponent },
+    { path: "signup/:role", component: SignupComponent },
+    {
+        path: "user",
+        loadChildren: () =>
+            import("./user/user.module").then((m) => m.UserModule),
+    },
+    {
+        path: "coach",
+        loadChildren: () =>
+            import("./coach/coach.module").then((m) => m.CoachModule),
+    },
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "**", redirectTo: "home", pathMatch: "full" },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginService } from './login/login.service';
+import { SignupService } from './signup/signup.service';
+import { UserModule } from './user/user.module';
+import { CoachModule } from './coach/coach.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    // UserModule,
+    // CoachModule
   ],
-  providers: [],
+  providers: [
+    // LoginService,
+    // SignupService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
